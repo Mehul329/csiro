12caee564cfdcc06d63057f132abd85f79dc61ee
Mehul329 <mehul.agarwal@student.unsw.edu.au>
2023-01-12 15:51:15 +1100

    scrunhing code

--- finder.py
+++ finder.py
@@ -1,31 +1,59 @@
 import numpy as np
 from sigpyproc.readers import FilReader as F
 import argparse
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 #%% Input
 a = argparse.ArgumentParser()
 a.add_argument('-f', type = str, help = 'Type the filename', default = '2018-06-27-04_14_17.fil')
-a.add_argument('-k', type = int, nargs = 3, help = 'Bins list: Start, End, Steps', default = [1,10,1])
-a.add_argument('-dm', type = int, nargs = 3, help = 'DM list: Start, End, Steps', default = [10,100,10])
+a.add_argument('-t_x', type = int, help = 'Give the value for scrunching', default = 2)
+a.add_argument('-k', type = int, nargs = 3, help = 'Bins list: Start, Factor, End', default = [2,1.1,5])
+a.add_argument('-dm', type = int, nargs = 3, help = 'DM list: Start, End, Steps', default = [1,46,1])
 a.add_argument('-fl_j', '--flattening_jump', type=int, help="Jump size to use when flattening the time series (def = 100", default=100)
-a.add_argument("-t", '--threshold', type=float, help='S/N threshold for selecting candidates (def = 8)', default=8)
+a.add_argument("-t", '--threshold', type=float, help='S/N threshold for selecting candidates (def = 8)', default=12)
 
 args = a.parse_args()
 filename = args.f #takes the filename
-k_start, k_end, k_space = args.k
+
+k_start, k_factor, k_end = args.k
+k_n = int(np.ceil((np.log(k_end/k_start)/np.log(k_factor))+1))
+k_end = k_start*k_factor**(k_n-1)
+kernel_lst = np.geomspace(k_start, k_end, k_n).astype(int) 
+kernel_lst = np.unique(kernel_lst) #take the list of bin sizes
+
 dm_start, dm_end, dm_space = args.dm
-kernel_lst = np.linspace(k_start, k_end, int(1+(k_end-k_start)/k_space)).astype(int) #take the list of bin sizes
 dm = np.linspace(dm_start, dm_end, int(1+(dm_end-dm_start)/dm_space)).astype(int) #take the list of possible dm's
+
+t_x = args.t_x
 
 #%%
 data = F(filename)
-nsamps = data.header.nsamples
-data = data.read_block(0, nsamps)
+'''
+def t_scrunch(data, t_x):
+    tot_nsamps = data.header.nsamples
+    block = data.read_block(0, tot_nsamps)
+    nchans, nsamps = block.shape
+    end_samp = nsamps//t_x * t_x
+    
+    return block[:, :end_samp].reshape(nchans, nsamps//t_x, t_x).mean(axis=-1), nsamps//t_x, nchans
+'''
 
+def t_scrunch(data, t_x):
+    n_chans = data.header.nchans
+    nsamps = data.header.nsamples
+    rem = nsamps % t_x
+    if rem != 0:
+        nsamps = nsamps - rem
+    data = data.read_block(0, nsamps)
+    nsamps = int(nsamps/t_x)
+    data = data.reshape([nsamps*n_chans,t_x])
+    data = np.average(data,axis = 1)
+    data = data.reshape(n_chans,nsamps)
+    return data, nsamps, n_chans
 
-n_chans = data.header.nchans
+data, nsamps, n_chans= t_scrunch(data, t_x)    
+
 max_f = data.header.fch1
 min_f = max_f + data.header.foff * (n_chans-1)
 possible_freq = np.linspace(max_f, min_f, n_chans) #different frequency channels
@@ -54,7 +82,7 @@ new_data = np.empty([n_chans,nsamps])
 threshold = args.threshold
     
 for i in range(len(possible_a)):
-    #print("DM is : ", dm[i])
+    print("DM is : ", dm[i])
     new_data[0] = data[0]
     for j in range(1, n_chans):
         freq = possible_freq[j]
@@ -73,19 +101,19 @@ for i in range(len(possible_a)):
     rms = flattened_sum.std()
 
     for kernel in kernel_lst:
-        #print("Kernel is: ", kernel)
+        print("Kernel is: ", kernel)
 
         mvaverage_arr = np.convolve(flattened_sum, np.ones(kernel), mode='valid')
         mvaverage_arr /= (rms * np.sqrt(kernel))
-        #plt.plot(mvaverage_arr)
-        #plt.show()
+        plt.plot(mvaverage_arr)
+        plt.show()
         peak_locs = mvaverage_arr > threshold
         snr = mvaverage_arr[peak_locs]
 
         SNRs.extend(list(snr))
-        Bins.extend(list(kernel * np.ones_like(snr)))
-        DMs.extend(list(dm[i] * np.ones_like(snr)))
-        Times.extend( list(np.arange(len(mvaverage_arr))[peak_locs]))
+        Bins.extend(list(kernel * t_x * np.ones_like(snr)))
+        DMs.extend(list(dm[i] * t_x * np.ones_like(snr)))
+        Times.extend(list(np.arange(len(mvaverage_arr))[peak_locs] * t_x))
         
 cands = np.column_stack([Times, Bins, DMs, SNRs])
 titles = np.array(['Times', 'Bins', 'DMs', 'SNRs'])
