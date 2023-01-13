5992c89fe2a4432946f12bd3625fec9c6e47ba3a
Mehul Agarwal <aga017@petrichor-login.cm.hpc>
2023-01-13 12:22:22 +1100

    Made the code as a command line input

--- runner.py
+++ runner.py
@@ -1,14 +1,16 @@
-import os
+import os, sys
 import argparse
 
 #%% Input
 a = argparse.ArgumentParser()
 a.add_argument('-f', type = str, help = 'Type the pointing name')
+a.add_argument('-c', type = str, help = 'Type the path to the code that you want to execute')
 args = a.parse_args()
 pointing = args.f+'/'
 
 #%%
 observations = os.listdir(pointing)
+#counter = 0
 '''
 #this is happening because there is . directory
 final_observations = []
@@ -17,7 +19,11 @@ for observation in temp_observations:
         final_observations.append(observation+'/')
 '''        
 for observation in observations:
-    beams = os.listdir(pointing+observation)
+    beams = os.listdir(pointing+observation + "/FB/")
     for beam in beams:
-        file = 'python3 code.py -f '+pointing+observation+beam
-        os.system(file)
+#        counter += 1 
+#        if counter > 2:
+#            sys.exit(0)
+        cmd = f"python3 {args.c} -f "+pointing+observation+"/FB/" +beam +"/"+ observation + ".fil"
+        print(f"Now executing the following command : {cmd}")
+        os.system(cmd)
