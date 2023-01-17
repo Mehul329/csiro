31d1b8a3d4786b34c0c48fda64d8f10bffc437ce
Mehul329 <mehul.agarwal@student.unsw.edu.au>
2023-01-17 11:25:23 +1100

    this take directly to filter bank

--- runner.py
+++ runner.py
@@ -1,10 +1,9 @@
-import os, sys
+import os
 import argparse
 import numpy as np
 #%% Input
 '''
 a = argparse.ArgumentParser()
-<<<<<<< HEAD
 a.add_argument('-f', type = str, help = 'Type the observation name')
 args = a.parse_args()
 pointing = args.f+'/'
@@ -23,29 +22,3 @@ for i in range(1):
     beam_dir = np.core.defchararray.add(np.repeat(obs_dir, beam_blocks.shape[0]), np.tile(beam_blocks, obs_dir.shape[0]))
     filter_bank_path = np.core.defchararray.add(func(beam_dir), '.fil')
     print(filter_bank_path)
-=======
-a.add_argument('-f', type = str, help = 'Type the pointing name')
-a.add_argument('-c', type = str, help = 'Type the path to the code that you want to execute')
-args = a.parse_args()
-pointing = args.f+'/'
-
-#%%
-observations = os.listdir(pointing)
-#counter = 0
-'''
-#this is happening because there is . directory
-final_observations = []
-for observation in temp_observations:
-    if observation[0] != '.':
-        final_observations.append(observation+'/')
-'''        
-for observation in observations:
-    beams = os.listdir(pointing+observation + "/FB/")
-    for beam in beams:
-#        counter += 1 
-#        if counter > 2:
-#            sys.exit(0)
-        cmd = f"python3 {args.c} -f "+pointing+observation+"/FB/" +beam +"/"+ observation + ".fil"
-        print(f"Now executing the following command : {cmd}")
-        os.system(cmd)
->>>>>>> 8dc0b30f16c1cdc85fc4d2b9e473ed0da1951b17
