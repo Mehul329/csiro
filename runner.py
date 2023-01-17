dc8e4331a26709700b1961e529d18761669c379b
Mehul329 <mehul.agarwal@student.unsw.edu.au>
2023-01-17 12:27:42 +1100

    Added the final commands to run the finder though slurm

--- runner.py
+++ runner.py
@@ -3,10 +3,14 @@ import argparse
 import numpy as np
 #%% Input
 
-a = argparse.ArgumentParser()
-a.add_argument('-n', type = int, help = 'Type the node number (1-32)')
-args = a.parse_args()
-node = args.n
+#a = argparse.ArgumentParser()
+#a.add_argument('-n', type = int, help = 'Type the node number (1-32)')
+#a.add_argument('-c', type = str, help = 'Type path of finder code')
+#args = a.parse_args()
+#node = args.n
+#code = args.c
+code = 'finder.py'
+node = 1
 
 basedir = '/Users/mehulagarwal/Desktop/Tape1/'
 observations = os.listdir(basedir)
@@ -20,4 +24,8 @@ beam_blocks = np.core.defchararray.add('/FB/beam_', beam_blocks)
 beam_blocks = np.core.defchararray.add(beam_blocks, '/')
 beam_dir = np.core.defchararray.add(np.repeat(obs_dir, beam_blocks.shape[0]), np.tile(beam_blocks, obs_dir.shape[0]))
 filter_bank_path = np.core.defchararray.add(func(beam_dir), '.fil')
-print(filter_bank_path)
+for filter_bank in filter_bank_path:
+    cmd = f"python3 {code} -f "+filter_bank
+    os.system(cmd)
+    print(f"Done : {np.array(cmd.split('/'))[np.array([-5,-4,-2])]}")
+
