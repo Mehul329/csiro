462d2a322b8ff2667e3d96fb72c86161bb89d723
Mehul Agarwal <aga017@petrichor-login.cm.hpc>
2023-01-17 14:35:17 +1100

    Small bug fix - capitalised beam to BEAM

--- runner.py
+++ runner.py
@@ -19,7 +19,7 @@ func = np.vectorize(lambda x:x+x.split('/')[5])
 
 beam_blocks = (np.linspace(1,11,11)+(node-1)*11).astype(int)
 beam_blocks = np.core.defchararray.zfill(beam_blocks.astype(str), 3)
-beam_blocks = np.core.defchararray.add('/FB/beam_', beam_blocks)
+beam_blocks = np.core.defchararray.add('/FB/BEAM_', beam_blocks)
 beam_blocks = np.core.defchararray.add(beam_blocks, '/')
 beam_dir = np.core.defchararray.add(np.repeat(obs_dir, beam_blocks.shape[0]), np.tile(beam_blocks, obs_dir.shape[0]))
 filter_bank_path = np.core.defchararray.add(func(beam_dir), '.fil')
