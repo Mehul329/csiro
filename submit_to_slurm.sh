6aabe6c5e4f21aae56ddcd5557b4489c599b35c7
Mehul Agarwal <aga017@petrichor-login.cm.hpc>
2023-01-17 15:04:12 +1100

    Changed the resource requirements to reflect real use case

--- submit_to_slurm.sh
+++ submit_to_slurm.sh
@@ -2,11 +2,11 @@
 #
 #SBATCH --job-name=find_ulps_in_smirf
 #SBATCH --ntasks=1
-#SBATCH --time=00:20:00
+#SBATCH --time=06:00:00
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=aga017@csiro.au
-#SBATCH --array=1-2
+#SBATCH --array=1-32
 
 tape=$1
 
