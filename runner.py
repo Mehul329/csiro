93642682891f3b7bbbbd6da6430bdc28fede7713
Mehul Agarwal <aga017@petrichor-login.cm.hpc>
2023-01-20 13:40:17 +1100

    changed the number of obs to 5

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import time
 import numpy as np
 #%% Input
 
@@ -15,6 +16,7 @@ basedir = tape
 observations = os.listdir(basedir)
 observations = observations[1:]
 obs_dir = np.core.defchararray.add(basedir, observations)
+obs_dir = obs_dir[0:5]
 func = np.vectorize(lambda x:x+x.split('/')[-4])
 
 beam_blocks = (np.linspace(1,8,8)+(node-1)*8).astype(int)
@@ -23,11 +25,13 @@ beam_blocks = np.core.defchararray.add('/FB/BEAM_', beam_blocks)
 beam_blocks = np.core.defchararray.add(beam_blocks, '/')
 beam_dir = np.core.defchararray.add(np.repeat(obs_dir, beam_blocks.shape[0]), np.tile(beam_blocks, obs_dir.shape[0]))
 filter_bank_path = np.core.defchararray.add(func(beam_dir), '.fil')
-for filter_bank in filter_bank_path[:5]:
+for filter_bank in filter_bank_path:
     if os.path.exists(filter_bank):
+        start = time.time()
         cmd = f"python3 {code} -f "+filter_bank
         os.system(cmd)
-        print(cmd)
+        end = time.time()
+        print(end-start, cmd)
 	#print(f"Done : {np.array(cmd.split('/'))[np.array([-5,-4,-2])]}")
     else:
         print(f"{filter_bank} does not exist")
