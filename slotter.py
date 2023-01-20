40bcb28111399fd76b07947f69c016c473889028
Mehul329 <mehul.agarwal@student.unsw.edu.au>
2023-01-20 20:16:40 +1100

    _

--- slotter.py
+++ slotter.py
@@ -25,6 +25,7 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 for icluster in uniq_clusters:
     cand = cands[icluster == clusters]
+    print(cand)
     #plt.plot(cand[:,0]/20, cand[:,3], '.')
     ax.scatter(cand[:,0]/20, 20*cand[:,1]/400, 20*cand[:,2]/40, '.')
 plt.show()
