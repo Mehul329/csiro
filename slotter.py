495c0d49d7fea141650a7c2858f0b9bed821cc7f
Mehul329 <mehul.agarwal@student.unsw.edu.au>
2023-01-20 19:25:27 +1100

    ML progress

--- slotter.py
+++ slotter.py
@@ -11,13 +11,13 @@ time = cands[:,0]/20
 boxcar = 20*cands[:,1]/400
 DM = 20*cands[:,2]/40
 SNR = cands[:,3]
-'''
+cls_obj = DBSCAN(eps=1.1, min_samples=1)
+
 #based on SNR and time
 #to check the scaling
 #plt.plot(cands[:,0], cands[:,3], '.')
 #plt.show()
 
-cls_obj = DBSCAN(eps=1.1, min_samples=1)
 clusters = cls_obj.fit(np.column_stack([time, SNR/max(SNR)])).labels_
 uniq_clusters = np.unique(clusters)
 nclusters = len(uniq_clusters)
@@ -26,7 +26,7 @@ for icluster in uniq_clusters:
     plt.plot(cand[:,0]/20, cand[:,3], '.')
 plt.show()
 print(max(clusters))
-'''
+
 
 '''
 #based on DM and time
@@ -34,7 +34,6 @@ print(max(clusters))
 #plt.plot(cands[:,0], cands[:,2], '.')
 #plt.show()
 
-cls_obj = DBSCAN(eps=1.1, min_samples=1)
 clusters = cls_obj.fit(np.column_stack([time, DM])).labels_
 uniq_clusters = np.unique(clusters)
 nclusters = len(uniq_clusters)
@@ -44,99 +43,34 @@ for icluster in uniq_clusters:
 plt.show()
 print(max(clusters))
 '''
-
+'''
 #based on Boxcar and time
 #to check the scaling
-plt.plot(cands[:,0], cands[:,1], '.')
-plt.show()
-'''
-cls_obj = DBSCAN(eps=1.1, min_samples=1)
-clusters = cls_obj.fit(np.column_stack([time, DM])).labels_
+#plt.plot(cands[:,0], cands[:,1], '.')
+#plt.show()
+
+clusters = cls_obj.fit(np.column_stack([time, boxcar])).labels_
 uniq_clusters = np.unique(clusters)
 nclusters = len(uniq_clusters)
 for icluster in uniq_clusters:
     cand = cands[icluster == clusters]
-    plt.plot(cand[:,0]/20, 20*cand[:,2]/40, '.')
+    plt.plot(cand[:,0]/20, 20*cand[:,1]/400, '.')
 plt.show()
 print(max(clusters))
 '''
 
 '''
-#plt.scatter(time, SNR, c=clusters, cmap='rainbow')
-#plt.show()
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-cls_obj = DBSCAN(eps=1, min_samples=1)
-clusters = cls_obj.fit(np.column_stack([time, DM, boxcar])).labels_
+#based on 3d
+clusters = cls_obj.fit(np.column_stack([time, boxcar, DM])).labels_
 uniq_clusters = np.unique(clusters)
 nclusters = len(uniq_clusters)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 for icluster in uniq_clusters:
     cand = cands[icluster == clusters]
-    plt.plot(cand[:,0]/20, 20*cand[:,2]/40, '.')
-    #plt.plot(cands_in_this_cluster[:,0]/20, cands_in_this_cluster[:,2]/2,'.')
+    ax.scatter(cand[:,0]/20, 20*cand[:,1]/400, 20*cand[:,2]/40, '.')
 plt.show()
-'''
-'''
-cls_obj = DBSCAN(eps=20, min_samples=1)
-clusters = cls_obj.fit(np.column_stack([time, DM])).labels_
-uniq_clusters = np.unique(clusters)
-nclusters = len(uniq_clusters)
-
-for icluster in uniq_clusters:
-    cands_in_this_cluster = cands[icluster == clusters]
-    plt.plot(cands_in_this_cluster[:,0]/20, cands_in_this_cluster[:,2]/2,'.')
-plt.show()
-'''
-'''
-plt.plot(cands[:,1], np.zeros_like(cands[:,0]),'.')
-plt.show()
-plt.plot(cands[:,1], cands[:,2],'.')
-plt.show()
-plt.plot(cands[:,2], np.zeros_like(cands[:,0]),'.')
-plt.show()
-plt.plot(cands[:,2], cands[:,0],'.')
-plt.show()
-#plt.plot(cands[:,0], cands[:,1], '.')
-#plt.show()
-'''
-'''
-dx = np.random.randint(1,100,30)
-dy = np.random.randint(1,100,30)
-dz = np.random.randint(1,100,30)
-R = 1.25 * dx
-for i in range(len(dy)):
-    sy = dx[i]/dy[i]
-    sz = dx[i]/dz[i]
-    x = np.transpose(cands[:,0]/dx[i])
-    y = np.transpose(cands[:,1]/sy)
-    z = np.transpose(cands[:,2]/sz)
-    cls_obj = DBSCAN(eps=R[i], min_samples=1)
-    clusters = cls_obj.fit(np.column_stack([x,y,z])).labels_
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x, y, z, '.', c=clusters, cmap='rainbow')
-    plt.show()
-    print(dx[i], dy[i], dz[i], R[i])
-    print(max(clusters))
-'''
-#%%
-'''
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x, y, z, '.' )#c=clusters)
-#ax.set_xlim([12800,13000])
-#ax.set_ylim([110450,110500])
-#ax.set_zlim([1000,1100])
-plt.show()
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-print(5)
-for i in range(len(clusters)):
-    ax.scatter(cand[0], cand[1], cand[2], colors[i])
-print(6)
-plt.show()
+print(max(clusters))
 '''
 
 #%%
