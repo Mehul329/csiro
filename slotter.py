d8d217285fb1e2a4fa9b38a3493313b2566f33cb
Mehul Agarwal <aga017@grus.atnf.CSIRO.AU>
2023-01-27 10:44:54 +1100

    perfecting the ml

--- slotter.py
+++ slotter.py
@@ -2,19 +2,17 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 import numpy as np
 import os
+import time
 #%%
 #this porgram should go into each observation (327) and cluster data for all beams at once
 #it take the location of observations number
-import time
-file1 = '/u/aga017/Desktop/output'
-beams = os.listdir(file1) 
-beams = beams[:4]
-cand_list = []
+
+intime = time.time()
 
 def cluster_each_beam(beam_cands, time_scale, dm_scale, box_scale, radius, beam_no):
     #dm_scale = within what range of DM would you consider it one cluster = 40
-    #time_scale = within what range of time would you consider it one cluster = 200
-    #box_scale = within what range of boxcar would you consider it one cluster = 100
+    #time_scale = within what range of time would you consider it one cluster = 50
+    #box_scale = within what range of boxcar would you consider it one cluster = 20
     #radius = radius under whihc you will mkae one cluster = 1.1    
     
     cls_obj = DBSCAN(eps=radius, min_samples=1)
@@ -25,56 +23,73 @@ def cluster_each_beam(beam_cands, time_scale, dm_scale, box_scale, radius, beam_
     start = time.time()
     clusters = cls_obj.fit(np.column_stack([times, dms, boxcars])).labels_
     
-    final_cands = []
+    cand_lst = []
     
     n_clusters = max(clusters)+1
-    print(time.time()-start)
-    plt.figure()
+    print(f'{beam_no} reduced from {len(beam_cands)} to {n_clusters} in {time.time()-start}')
+    #plt.figure()
     for icluster in range(n_clusters):
         cand = beam_cands[icluster == clusters]
         final_cand = cand[np.where(cand[:,3]==np.max(cand[:,3]))[0]][0]
-        plt.plot(final_cand[0], final_cand[1], '.')
+        #plt.plot(final_cand[0], final_cand[2], '.')
         final_cand = np.append(final_cand, beam_no)
-        final_cands.append(final_cand)
-    plt.show()
-    final_cands = np.array(final_cands)
-    return final_cands
-    
-beam_cands = np.loadtxt(file1+'/BEAM_176.txt', dtype='str')
-beam_cands = beam_cands[1:,:].astype(float)
-cluster_each_beam(beam_cands, 200, 40, 100, 1.1, 176)
+        cand_lst.append(final_cand)
+    #plt.show()
+    cand_lst = np.array(cand_lst)
+    return cand_lst
 
-'''
-cls_obj = DBSCAN(eps=1.1, min_samples=1)
-for i in range(len(beams)):
-    beam_no = int(beams[i].split('_')[1].split('.')[0])
+
+file1 = '/u/aga017/Desktop/output'
+beams = os.listdir(file1) 
+#beams = beams[173:176]
+
+filtered_beams = []
+for beam_cand in beams:
+    beam_no = int(beam_cand.split('.')[0].split('_')[1])
+    beam_cand = np.loadtxt(file1+'/'+beam_cand, dtype='str')
+    beam_cand = beam_cand[1:,:].astype(float)
+    filtered_beams.extend(cluster_each_beam(beam_cand, 50, 40, 20, 1.1, beam_no))
     
-    print(beams[i])
-    cands = np.loadtxt(file1+'/'+beams[i], dtype='str')
-    cands = cands[1:,:].astype(float)
-    time = cands[:,0]
-    DM = cands[:,2]
-    clusters = cls_obj.fit(np.column_stack([time, DM])).labels_
+filtered_beams = np.array(filtered_beams)
+
+#%%
+def cluster_all_beams(filtered_beams, time_scale, dm_scale, beam_scale, radius):
+    #dm_scale = within what range of DM would you consider it one cluster = 40
+    #time_scale = within what range of time would you consider it one cluster = 50
+    #beam_scale = within what range of beams would you consider it one cluster = 4
+    #radius = radius under whihc you will mkae one cluster = 1.1  
+    cls_obj = DBSCAN(eps=radius, min_samples=1)
+    times = filtered_beams[:,0] / time_scale
+    dms = filtered_beams[:,2] / dm_scale
+    beams = filtered_beams[:,-1] / beam_scale 
+
+    start = time.time()
+    clusters = cls_obj.fit(np.column_stack([times, dms, beams])).labels_   
+    
     final_cands = []
+    
     n_clusters = max(clusters)+1
-    plt.figure()
-    plt.plot(time, DM, '.')
-    plt.show()
-    
+    count = 0
+    #plt.figure()
     for icluster in range(n_clusters):
-        cand = cands[icluster == clusters]
-        final_cand = cand[np.where(cand[:,3]==np.max(cand[:,3]))[0]]
-        final_cand = np.append(final_cand, beam_no)
-        final_cands.append(final_cand)
-    print(n_clusters)
-    final_cands = np.array(final_cands)
-    final_cands[:, 3] *= 20
-    cand_list.extend(final_cands)
-    plt.plot(final_cands[:,0]*20, final_cands[:,2]*20, '.')
-    plt.xlim([0,600000])
-    plt.show()
+        cand = filtered_beams[icluster == clusters]
+        min_beam = np.min(cand[:,-1])
+        max_beam = np.max(cand[:,-1]) 
+        #plt.plot(cand[:,-1], cand[:,0], '-')
+        if max_beam - min_beam < 2:
+            candidate = cand[cand[:,-2] == np.max(cand[:,-2])][0]
+            final_cands.append(candidate)
+            count+=1
+            plt.plot(candidate[0], candidate[2], '-')
+    #plt.ylim([0,250])
+    #plt.show()
+    print(f'Reduced from {len(filtered_beams)} to {n_clusters} to {count} in {time.time()-start}')
     
-    break
-cand_list = np.array(cand_list)
-#np.save('/u/aga017/Desktop/beams_clustered', cand_list)
-'''
+    return final_cands
+
+final_cands = np.array(cluster_all_beams(filtered_beams, 100, 40, 5, 1.1))
+np.save('/u/aga017/Desktop/final_cands', final_cands)
+header = np.array(['Time', 'Boxcar', 'DM', 'SNR', 'BEAM'])
+final_cands = np.row_stack([header, final_cands])
+np.savetxt('/u/aga017/Desktop/final_cands.txt', final_cands, fmt = '%s')
+print(time.time() - intime)
