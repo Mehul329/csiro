2f9721cad0da60898c5feea3a96f8993c26cfd53
Mehul329 <mehul.agarwal@student.unsw.edu.au>
2023-01-27 23:23:16 +1100

    memory change

--- submit_to_slurm_slotter.sh
+++ submit_to_slurm_slotter.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=find_ulps_in_smirf
 #SBATCH --ntasks=1
 #SBATCH --time=02:00:00
-#SBATCH --mem-per-cpu=3GB
+#SBATCH --mem-per-cpu=10GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=aga017@csiro.au
 #SBATCH --array=1-33
