e7be4843cbe4faf1c6693171c82dc31a6a2ffd66
Mehul329 <mehul.agarwal@student.unsw.edu.au>
2023-01-28 17:33:31 +1100

    to run on slurm

---
+++ submit_to_slurm_plotter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=find_ulps_in_smirf
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --mem-per-cpu=10GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=aga017@csiro.au
+#SBATCH --array=6
+
+tape=$1
+
+srun /home/aga017/codes/csiro/launch_job_slotter.sh $SLURM_ARRAY_TASK_ID $tape
